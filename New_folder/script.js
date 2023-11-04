b38b4edc395e37b94f15f5ae8e26da14eda25542
Manoj7855 <manojaeroaoota56@gmail.com>
2023-11-04 12:48:09 +0530

    inital

---
+++ New_folder/script.js
@@ -0,0 +1,74 @@
+// alert("Welcomtojs")
+/*console.log(123455);
+console.log(12.3);
+console.log(true);
+console.log(true);
+console.log([78,89,56,87]);
+console.log({fname:'manoj',age:25});
+console.table({fname:'manoj',age:25});
+console.error("Customm Sample error");
+console.warn("Customm Sample error");
+console.clear();
+console.time("Timer");
+for(i=0;i<10;i++)
+{
+    console.log(i);
+}
+
+console.timeEnd("Timer"); */
+
+// var a=20;
+// var b=20;
+// console.log(a+b);
+
+// if(true)
+// {
+//     var msg="WelcomeManoj";
+// }
+// console.log(msg)
+
+/*var b=25;
+console.log(b);
+b=40;
+console.log(b); */
+
+// let b=2;
+// console.log(b);
+// b=3;
+// console.log(b);
+
+// const b=25;
+// console.log(b);
+// b=35;  //constant error 
+// comnsole.log(b)
+
+
+// const student={'name':'Ram',"age":12};
+// console.table(student);
+// console.log(student.name);
+// student.name="Manoj";
+// console.table(student);
+
+//2 Data Types in JavaScript:
+/*
+Number eg: 1.25,25
+Boolean eg: True,False
+Null
+Undefined
+symbols
+it is a Dynamic Programming*/
+
+var a=25;
+console.log(typeof a);
+
+var fname="Manoj";
+console.log(typeof fname);
+
+var ismarried=true;
+console.log(typeof ismarried);
+
+var phone=null;
+console.log(typeof phone);
+
+
+
